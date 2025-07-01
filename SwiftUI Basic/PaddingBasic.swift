05f2ac005738cf09b38c01c80f2cdaca9f565f49
jungseok-corine <nonehour@naver.com>
2025-07-01 17:30:48 +0900

    SwiftUI Basic 공부 시작

---
+++ SwiftUI Basic/PaddingBasic.swift
@@ -0,0 +1,18 @@
+//
+//  PaddingBasic.swift
+//  SwiftUI Basic
+//
+//  Created by 오정석 on 1/7/2025.
+//
+
+import SwiftUI
+
+struct PaddingBasic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaddingBasic()
+}
